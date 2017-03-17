a82c6993842f26ad2df83e480e47de616b10adc8
John Losito <lositojohnj@gmail.com>
2017-03-17 12:06:08 -0400

    hulton user able to put in escape characters for service type

--- part3/hulton/add_remove/back_service.php
+++ part3/hulton/add_remove/back_service.php
@@ -3,12 +3,13 @@ $data_json = file_get_contents('php://input');
 //var_dump($data_json);
 $data = json_decode($data_json, true);
 
-$hotelid = (int)$data['hotelid'];
-$stype = $data['stype'];
-$sprice = $data['sprice'];
-$request = $data['request'];
-
 include('../../config.php');
+
+$hotelid = (int)$data['hotelid'];
+$stype = mysqli_real_escape_string($conn, $data['stype']);
+$sprice = $data['sprice'];
+
+$request = $data['request'];
 
 if(strcmp($request, "add") == 0){
   $sql = "insert into SERVICE (HotelID, SType, SPrice) values ($hotelid, '$stype', $sprice)";
